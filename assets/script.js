7a313491a51ac3b2fd10e0bae1498b75e43d5345
Scrysis <Scrysis@users.noreply.github.com>
2023-11-13 18:36:39 -0800

    base commit

---
+++ assets/script.js
@@ -0,0 +1,12 @@
+
+
+var cardDisplay = $("#bottomDisplay"); // base element for creating the cards
+
+var userCity = [];  //empty; will be filled by a modified user response 
+
+var searchButton = $('<button>');
+
+// %20 to indicate whitespace in the api call for city names with multiple words
+
+/* Need code for taking user input and turning it into search terms. */
+
